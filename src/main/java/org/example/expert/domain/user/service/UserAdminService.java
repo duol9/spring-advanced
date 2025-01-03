da3954085fbdb301779abc822010d05cb37e9c8a
duol9 <lhyy0195@gmail.com>
2025-01-03 16:33:42 +0900

    Refactor: 유저 조회 로직 UserService로 위임해 책임 분리
    - UserService의 findUserOrThrow 메서드로 조회 및 예외처리 로직을 위임해 재사용성 향상
    - SRP을 준수해 서비스 계층 간 역할을 명확히 분리

--- src/main/java/org/example/expert/domain/user/service/UserAdminService.java
+++ src/main/java/org/example/expert/domain/user/service/UserAdminService.java
@@ -2,11 +2,9 @@ package org.example.expert.domain.user.service;
 
 import lombok.RequiredArgsConstructor;
 
-import org.example.expert.domain.common.exception.InvalidRequestException;
 import org.example.expert.domain.user.dto.request.UserRoleChangeRequest;
 import org.example.expert.domain.user.entity.User;
 import org.example.expert.domain.user.enums.UserRole;
-import org.example.expert.domain.user.repository.UserRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -14,11 +12,11 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class UserAdminService {
 
-	private final UserRepository userRepository;
+	private final UserService userService;
 
 	@Transactional
 	public void changeUserRole(long userId, UserRoleChangeRequest userRoleChangeRequest) {
-		User user = userRepository.findById(userId).orElseThrow(() -> new InvalidRequestException("User not found"));
+		User user = userService.findUserOrThrow(userId);
 		user.updateRole(UserRole.of(userRoleChangeRequest.getRole()));
 	}
 }
